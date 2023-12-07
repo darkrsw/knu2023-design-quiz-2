6d08fe51639512becec2ba5b057c456a729d7df5
Dongsun Kim <darkrsw@gmail.com>
2023-12-07 12:58:58 +0900

    updated test case.

--- test_analyzer1.py
+++ test_analyzer1.py
@@ -3,8 +3,8 @@ from invoke_analyzer import collect_uninvoked_methods
 test_input_path = "./src"
 
 def test_methods_uninvoked():
-    expected = { 
-                "C": {},
+    expected = {
+                "C": set(),
                 "A": {"__init__", "func1"},
                 "B": {"f1", "f2"}
       }
